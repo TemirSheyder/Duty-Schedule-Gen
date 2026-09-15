f50bb6e55323da63e56526a645bcc97500c4d53a
TemirSheyder <146768013+TemirSheyder@users.noreply.github.com>
2026-09-15 05:45:16 +0300

    Update params.js

--- js/params.js
+++ js/params.js
@@ -111,7 +111,7 @@ export function readVisualParams() {
       clip: chk("pBarClip"),
     },
     art: {
-      text: "t.me/ARTemaARTema",
+      text: "t.me/ARTtemaARTtema",
       fontFamily: artPicker.fontFamily, fontStyle: artPicker.fontStyle, align: "left",
       size: fsv("pArtSize"), tracking: num("pArtTracking"),
       h: mmv("pArtH"),
